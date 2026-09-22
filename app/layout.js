f4a63d546ea0ed3a63afcd632ddd1be22b396f25
abhisharma82957 <abhisharma82957@gmail.com>
2026-09-22 12:02:43 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
@@ -158,6 +159,19 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="font-body antialiased">
+            <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-M5QBRB6LG7"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-M5QBRB6LG7');
+  `}
+</Script>
         <a
           href="#main"
           className="sr-only focus:not-sr-only focus:absolute focus:left-4 focus:top-4 focus:z-50 focus:bg-ink focus:px-4 focus:py-2 focus:text-ivory"
